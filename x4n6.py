a9d716b33c623b1a18527c674f069b635927d73a
bl4ck0ut <cpfaff@ubuntu.(none)>
2016-08-31 12:38:10 +0000

    adding a test file

---
+++ x4n6.py
@@ -0,0 +1,24 @@
+import Tkinter
+import tkMessageBox
+import subprocess
+from Tkinter import *
+from ttk import Frame, Button, Style
+
+master = Tk()
+master.resizable(width=True, height=True)
+master.geometry('200x100')
+var = StringVar(master)
+var.set("arena1") # initial value
+
+option = OptionMenu(master, var, "arena1", "arena2", "arena3", "arena4")
+option.config(bg = "GREEN")
+option.pack()
+
+def arena():
+   subprocess.Popen(["cp", "./"+var.get()+"/rule/"+ var.get()+".rule", "/etc/nsm/rules/local.rules"])
+   subprocess.Popen(["sudo","python","./"+var.get()+"/"+var.get()+".py"])
+
+button = Button(master, text="Run!!", command=arena)
+button.pack()
+
+mainloop()
